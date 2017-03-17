01a3323db5c64b6df719e34818105328334bec6f
nrats <nishantrathi100@gmail.com>
2017-03-17 15:41:45 -0700

    changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from github import Github
 
 app = Flask(__name__)
+gitRepoURL=''
 
 @app.route("/")
 def hello():
@@ -12,9 +13,10 @@ def hello():
 def getConfigFile(filename):
 	gitClient = Github();
 	gitRepo = gitClient.get_repo('');
-	file_content = repo.get_file_contents(removeExtension(name)+'.yml');
+	file_content = repo.get_file_contents(name);
 	return file_content;
 	
 
 if __name__ == "__main__":
+    gitRepoURL = sys.argv[1].replace('https://github.com/','')
     app.run(debug=True,host='0.0.0.0')
