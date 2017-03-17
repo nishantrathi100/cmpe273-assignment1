db8adb0039f4fd64872ff3e4339258074861b3c0
nrats <nishantrathi100@gmail.com>
2017-03-17 15:05:35 -0700

    initial changes for part II of assignment

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from github import Github
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from Dockerized Flask App!!"
+
+
+@app.route("/v1/<filename>")
+def getConfigFile(filename):
+	gitClient = Github();
+	gitRepo = gitClient.get_repo('');
+	file_content = repo.get_file_contents(removeExtension(name)+'.yml');
+	return file_content;
+	
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
