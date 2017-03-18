f71491003500f2e22398d6d0eb981e2e5d4a228f
nrats <nishantrathi100@gmail.com>
2017-03-17 17:30:46 -0700

    changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
 def getConfigFile(filename):
 	gitClient = Github();
 	gitRepo = gitClient.get_repo(gitRepoURL);
-	file_content = repo.get_file_contents(name);
+	file_content = gitRepo.get_file_contents(name);
 	return file_content;
 	
 
